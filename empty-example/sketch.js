c99c0356cdc76731ca00146e14eb4cd2151c64c6
Kennedy <kennedyhchung@gmail.com>
2021-02-09 00:32:00 -0800

    added background to canvas

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,14 +1,14 @@
 function setup() {
   // CREATE TOP FEATURE BAR
-  let page = createDiv().size(windowWidth, windowHeight * 0.075);
-  page.position(0, 0)
-  let col = color(25, 23, 200, 50)
+  let bar = createDiv().size(windowWidth, windowHeight * 0.046);
+  bar.position(0, 0)
+  let col = color(0, 0, 0,100)
 
   // CHANGE FEATURE BAR STYLE
-  page.style("background-color", col);
-  page.style("display", "flex");
-  page.style("justify-content", "start");
-  page.style("align-items", "center");
+  bar.style("background-color", col);
+  bar.style("display", "flex");
+  bar.style("justify-content", "start");
+  bar.style("align-items", "center");
 
   // CREATE TOP BAR BUTTONS
   let button1 = createButton("HELP");
@@ -18,22 +18,27 @@ function setup() {
   changeButtonPadding(button1);
   changeButtonPadding(button2);
 
-  button1.parent(page);
-  button2.parent(page);
+  button1.parent(bar);
+  button2.parent(bar);
   
   // CREATE COURT CANVAS
-  let court = createCanvas(windowWidth, windowHeight- windowHeight * 0.075);
-  court.position(0, windowHeight * 0.075);
+  let court = createCanvas(windowWidth, windowHeight);
+  court.position(0, 0);
+  console.log(windowWidth);
 
   // button = createImg('mod.png');
   // button.position(19, 19);
   // button.mousePressed(changeBG);
   console.log(windowHeight * 0.075);
+
+  img = loadImage("./CodebaseCourtWhite.jpg");
 }
 
 function draw() {
   // put drawing code here
   background(250, 250, 100);
+  image(img, 0, 0);
+  img.resize(windowWidth, windowHeight);
 }
 
 function changeButtonPadding(butt) {
